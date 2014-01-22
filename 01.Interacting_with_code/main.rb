368fe4ea3235585e1a138d37796e8affcf4c59fc
mathieuripert <mathieu.ripert@gmail.com>
2014-01-22 11:38:01 -0800

    solution for 1.

--- 01.Interacting_with_code/main.rb
+++ 01.Interacting_with_code/main.rb
@@ -6,22 +6,31 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db,table_name)
   # your code here
+  query = "select count(*) from #{table_name}"
+  return db.execute(query)[0][0]
 end
 
 puts "There are #{number_of_rows(db,"Artist")} artists"
 puts "There are #{number_of_rows(db,"Track")} tracks"
 puts "There are #{number_of_rows(db,"Album")} albums"
-puts "There are #{number_of_rows(db,"Playlist")} playlist"
 puts "There are #{number_of_rows(db,"Genre")} genres"
 puts "There are #{number_of_rows(db,"MediaType")} media types"
-puts "There are #{number_of_rows(db,"PlaylistTrack")} playlist tracks"
 
 
 # returns all the artists sorted by alphabetical order.
-artists = # your code here
+query = "select name from Artist order by name DESC"
+artists = db.execute(query)
+#puts artists
 
 # returns all the love songs
-love_songs = # your code here
+query = "select name from Track where name like '%love%'"
+love_songs = db.execute(query)
+puts "There are #{love_songs.count} love songs"
+#puts love_songs
+
 
 # returns all the tracks that are longer than 5 minutes.
-long_tracks = # your code here
+query = "select name from Track where milliseconds > 60*5"
+long_tracks = db.execute(query)
+puts "There are #{long_tracks.count} long tracks"
+
